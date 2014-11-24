6b768241cb6b1d4e32d9685ee9997009764bf0be
Alex Galletti <agalletti@bluechipww.com>
2014-11-24 16:39:42 -0600

    geoip setup

--- app/configuration/environment.php
+++ app/configuration/environment.php
@@ -3,7 +3,9 @@
 return [
     'monolog.logfile'   => __DIR__.'/../storage/debug.log',
     'twig.path'         => __DIR__.'/../views',
-
+    'twig.options'    => [
+        'cache' => __DIR__.'/../storage/cache'
+    ],
     'maxmind_download'  => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz',
     'maxmind_database'  => __DIR__.'/../storage/database.mmdb',
 ];
