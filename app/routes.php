4de3cdfaf1bec5da901bc9c2bcd41a487f50d364
Alex Galletti <agalletti@bluechipww.com>
2014-11-24 16:39:42 -0600

    geoip setup

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,25 @@
 <?php
 
 use Silex\Application;
+use MaxMind\Db\Reader;
 
-$app['controllers']->value('locale', 'en');
+$app->match('api/1/{ip}', function (Application $app) {
 
-$app->match('{locale}', function (Application $app) {
-    return $app['twig']->render('index.twig');
-});
+    $ip = $app['request']->get('ip', false);
+
+    try {
+        $reader = new Reader(__DIR__.'/storage/database.mmdb');
+
+        $results = $reader->get($ip);
+
+        $reader->close();
+    } catch (Exception $e) {
+        return $app->json(['results' => false, 'error' => 'Invalid IP address specified.']);
+    }
+
+    if (empty($results)) {
+        return $app->json(['results' => false, 'error' => 'There is no data for the specified location.']);
+    }
+
+    return $app->json(compact('results'));
+})->value('ip', false);
