f77df20c6dea7131b2cf96122dd0e58fcac103a2
Alex Galletti <agalletti@bluechipww.com>
2014-12-09 11:27:51 -0600

    forgot to add maxmind config

--- app/configuration/environment.php
+++ app/configuration/environment.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
-    'monolog.logfile' => __DIR__.'/../storage/debug.log',
-    'twig.path' => __DIR__.'/../views',
+    'monolog.logfile'   => __DIR__.'/../storage/debug.log',
+    'twig.path'         => __DIR__.'/../views',
+
+    'maxmind_download'  => 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz',
+    'maxmind_database'  => __DIR__.'/../storage/database.mmdb',
 ];
